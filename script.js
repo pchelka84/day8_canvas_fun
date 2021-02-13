0bc3805fb415bfce3a2a930ec90a2e9268dc8d75
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-12 21:33:35 -0500

    Set up basic drawing

--- script.js
+++ script.js
@@ -2,11 +2,35 @@ const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.strokeStyle = 'BADA55';
+ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
-ctx.lineCar = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 10;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+
+function draw(e) {
+  // Stop the fn from running when they are not moused down
+  if (!isDrawing) return;
+  console.log(e);
+  ctx.beginPath();
+  // Start from
+  ctx.moveTo(lastX, lastY);
+  // Go to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+}
+
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  // Start drawing where we want to
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+
+});
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', ()=> isDrawing = false);
+canvas.addEventListener('mouseout', ()=> isDrawing = false);
 
